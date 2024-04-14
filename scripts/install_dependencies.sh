06bde2f634211f6e8e81567c1df9a0dd7949ef54
chandrapal-7773 <166910560+chandrapal-7773@users.noreply.github.com>
2024-04-14 08:55:18 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,8 @@
 
-# install_dependencies.sh
-#!/bin/bash
-cd /var/www/html/react-app
-npm install  # or yarn install
+echo 'install_dependencies.sh: ' >> //var/www/html/react-app/deploy.log
+
+echo 'cd /var/www/html/react-app' >> /var/www/html/react-app/deploy.log
+cd /var/www/html/react-app >> /var/www/html/react-app/deploy.log
+
+echo 'npm install' >> /var/www/html/react-app/deploy.log 
+npm  >> //var/www/html/react-app/deploy.log
