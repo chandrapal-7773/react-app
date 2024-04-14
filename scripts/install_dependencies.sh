23ea49caf7b10ed46c680a98562d6d85ff35993b
chandrapal-7773 <166910560+chandrapal-7773@users.noreply.github.com>
2024-04-14 09:49:13 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,2 @@
 
-echo 'install_dependencies.sh: ' >> //var/www/html/deploy.log
-
-echo 'cd /var/www/html' >> /var/www/html/deploy.log
-cd /var/www/html >> /var/www/html/deploy.log
-
-echo 'npm install' >> /var/www/html/deploy.log 
-npm install >> //var/www/html/deploy.log
+rm -rf /var/www/html/*
