89b38f32e1767881c8502c5de697eb927a7dad35
chandrapal-7773 <166910560+chandrapal-7773@users.noreply.github.com>
2024-04-14 09:58:59 +0530

    Update configure_nginx.sh

--- scripts/configure_nginx.sh
+++ scripts/configure_nginx.sh
@@ -1,16 +1,1 @@
-# configure_nginx.sh
-#!/bin/bash
-# Configure Nginx to serve the React app
-cat > /etc/nginx/conf.d/react-app.conf <<EOF
-server {
-    listen 80;
-    server_name your_domain.com;
 
-    root /var/www/html/react-app/build;
-    index index.html;
-
-    location / {
-        try_files \$uri /index.html;
-    }
-}
-EOF
