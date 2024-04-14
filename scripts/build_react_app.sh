2bcd7334ad6593eacf0ffebb649776a3f57e5ac4
chandrapal-7773 <166910560+chandrapal-7773@users.noreply.github.com>
2024-04-14 08:55:39 +0530

    Update build_react_app.sh

--- scripts/build_react_app.sh
+++ scripts/build_react_app.sh
@@ -1,4 +1,10 @@
 # build_react_app.sh
 #!/bin/bash
 cd /var/www/html/react-app
-npm run build  # or yarn build
+npm run build  # or yarn buildecho 'build_react_app.sh: ' >> //var/www/html/react-app/deploy.log
+
+echo 'cd /var/www/html/react-app' >> /var/www/html/react-app/deploy.log
+cd /var/www/html/react-app >> /var/www/html/react-app/deploy.log
+
+echo 'npm install' >> /var/www/html/react-app/deploy.log 
+npm run build >> //var/www/html/react-app/deploy.log
