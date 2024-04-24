4bfe627e76f29ce9b72e051964efd8e5f25b4fa4
chandrapal-7773 <166910560+chandrapal-7773@users.noreply.github.com>
2024-04-24 17:29:46 +0530

    Update change_permissions.sh

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,5 +1,11 @@
-# start_nginx.sh
+# build_react_app.sh
 #!/bin/bash
-# Start Nginx
+
+echo 'cd /var/www/html' >> /var/www/html/WordPress/deploy.log
+cd /var/www/html/WordPress>>/var/www/html/WordPress/deploy.log
+
+echo 'npm install' >>/var/www/html/WordPress/deploy.log 
+npm install >> /var/www/html/WordPress/deploy.log
+npm run build  # or yarn buildecho 'build_react_app.sh: ' >> //var/www/html/deploy.log
 systemctl restart nginx
 
