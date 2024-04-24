9b38bb9003f921b1a3f39be01356df4d54bcc572
chandrapal-7773 <166910560+chandrapal-7773@users.noreply.github.com>
2024-04-24 17:30:29 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,12 +1,6 @@
 # build_react_app.sh
 #!/bin/bash
-
-echo 'cd /var/www/html' >> /var/www/html/deploy.log
-cd /var/www/html>> /var/www/html/deploy.log
-
-echo 'npm install' >> /var/www/html/deploy.log 
-npm install >> /var/www/html/deploy.log
-npm run build  # or yarn buildecho 'build_react_app.sh: ' >> //var/www/html/deploy.log
+systemctl restart nginx
 
 
 
