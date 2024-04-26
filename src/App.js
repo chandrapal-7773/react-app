51569f0b6c8274711216389b67f3c41ebc5bd38d
chandrapal-7773 <166910560+chandrapal-7773@users.noreply.github.com>
2024-04-26 21:04:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Majja Marro
+         Hello Everyone
         </a>
       </header>
     </div>
